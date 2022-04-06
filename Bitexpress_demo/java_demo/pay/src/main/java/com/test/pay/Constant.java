5aca8955b4ca02a8dbebf79bf5dada8062711683
BitExpress-api <89496010+BitExpress-api@users.noreply.github.com>
2022-04-06 11:07:00 +0800

    Update Constant.java

--- Bitexpress_demo/java_demo/pay/src/main/java/com/test/pay/Constant.java
+++ Bitexpress_demo/java_demo/pay/src/main/java/com/test/pay/Constant.java
@@ -6,7 +6,7 @@ package com.test.pay;
  * @Version 1.0
  */
 public class Constant {
-    public final static String URL_ = "https://merchant-api-ssl.bitexpress.me/"; // 域名
+    public final static String URL_ = "https://merchant-api-ssl.bitexpress.cc/"; // 域名
     public final static String key = "73A8946D4ED1AD34ADA3F503601D3833"; // HMAC_SHA1加密算法的key(密钥) <由 Bitexpress 分配,在app-API配置(pc商户后台--账户中心--API安全--密钥) 中获取>
     public final static String app_id = "41043343"; // 商户帐号ID(APPID) <由 Bitexpress 分配,在app-API配置(pc在商户后台--账户中心--商户信息) 中获取>
     public final static String merchant_no = "9357"; //商户号(UID) <由 Bitexpress 分配,在app-API配置(pc商户后台--账户中心--商户信息) 中获取>
